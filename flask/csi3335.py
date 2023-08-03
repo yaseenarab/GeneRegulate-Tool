ef5a4e68bc69d9f1c88b84884c1f6e68fa889962
yaseenarab <117604415+yaseenarab@users.noreply.github.com>
2023-08-03 01:30:07 -0500

    Update csi3335.py

--- flask/csi3335.py
+++ flask/csi3335.py
@@ -1,5 +1,1 @@
-#con = { 'host':'localhost', 'user':'cyrilpillai', 'password':'YES', 'database': 'baseball' }
-con = { 'host':'localhost', 'user':'yassenarab', 'password':'YA2002ya', 'database': 'baseball'}
 
-
-#con = { 'host' : 'localhost', 'user' : 'web', 'password' : 'mypass', 'database' : 'baseball' }
